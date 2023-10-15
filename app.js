34e0ea407508cfe59c845b7d70892c58d68b10e9
vitorkolle <vitorpaeskolle@gmail.com>
2023-10-15 04:15:45 -0300

    página inicial finalizada

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 'use strict'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function lua(){
+    window.location.href = "./pags/lua.html"
+}
+
+function mar(){
+    window.location.href = "./pags/mar.html"
+}
+
+function pic(){
+    window.location.href = "./pags/pic.html"
+}
