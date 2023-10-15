08175e84fd895cabe97a9cbe4c32b3b90920f00f
vitorkolle <vitorpaeskolle@gmail.com>
2023-10-15 16:13:23 -0300

    desenvolvimento do uso da api

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
 'use strict'
 
+const data_pic = document.getElementById('data-pic')
 
+ const urlAPiPicDia = `https://api.nasa.gov/planetary/apod?api_key=aPYdTFBOJ3plq0jbUcveZsqBmASwOcl7RksDVUB8&title&date=${data_pic}`
 
+ async function pegarImagem(){
+    const responseP = await fetch(urlAPiPicDia)
+    const dadosP = await responseP.json()
+    console.log(dadosP) 
+ }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ pegarImagem()
+ 
+ 
 function lua(){
     window.location.href = "./pags/lua.html"
 }
