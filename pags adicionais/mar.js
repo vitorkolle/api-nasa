29cc857905566c70701a9876bb219e56a46581de
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2023-10-20 11:46:55 -0300

    print site

--- pags adicionais/mar.js
+++ pags adicionais/mar.js
@@ -5,34 +5,26 @@ async function pegarImagemM(){
     const responseM = await fetch(urlAPiPicMarte)
     const dadosM = await responseM.json()
     const imgM = dadosM.latest_photos
-    let imagensMarte = []
+    const imagensMarte = []
     
     imgM.forEach(imagemMarte => {
-        imagensMarte.push(imagemMarte.img_src)        
-    });
-
-    console.log(imagensMarte)
-
+     imagensMarte.push(imagemMarte.img_src)         
+    })
     return imagensMarte
+
 }
-
 function criarImgM(imagensMarte){
-    
-    console.log(imagensMarte)
-
     const galeriaM = document.getElementById('galeriaM')
     
     imagensMarte.forEach(imagemMarte => {
-        const i = -1
+        const i = 0
         const imgMar = document.createElement('img')
         imgMar.src = imagemMarte[i]
         galeriaM.appendChild(imgMar)      
-    });   
+    });    
 }
-
 async function mostrarImgM(){
     const imgMar = await pegarImagemM()
      criarImgM(imgMar)   
  }
-
- criarImgM()
+criarImgM()
